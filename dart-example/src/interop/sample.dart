bc17beae30bea2df5b47eec6d097769cf63e6885
Dmitry Buzdin <buzdin@gmail.com>
2012-10-28 12:49:14 +0200

    EventBridge extracted as a separate class;

--- dart-example/src/interop/sample.dart
+++ dart-example/src/interop/sample.dart
@@ -4,18 +4,33 @@
 #import("../../packages/JsonObject/JsonObject.dart");
 #import('../../packages/js/js.dart', prefix: 'js');
 
+class EventBridge {
+
+// TODO convert response from JSON to Map and pass to callback
+  void subscribe(String eventId, Function callback) {
+    var bridge = js.context.$bridge;
+    bridge.subscribe(eventId, new js.Callback.many(callback));
+  }
+
+  void publish(String eventId, Map message, Function callback) {
+    js.scoped(() {
+      var json = js.map(message);
+      var bridge = js.context.$bridge;
+      bridge.publish(eventId, json, new js.Callback.many(callback));
+    });
+  }
+
+}
+
 void main() {
 
   callback(_) {
     js.scoped(() {
-      js.context.alert('Hello from Dart via JavaScript.');
+      js.context.console.log('Hello from Dart via JavaScript.');
     });
   }
 
-  js.scoped(() {
-    var json = js.map({'value': 'Hello from Dart'});
-    var bridge = js.context.$bridge;
-    bridge.publish('dartEvent', json, new js.Callback.once(callback));
-  });
+  var eventBridge = new EventBridge();
+  eventBridge.publish('dartEvent', {'value': 'Hello from Dart'}, callback);
 
 }
